d8d0534ce6e12a92d3ab931886cc6028ed1bca13
doubleboss00 <doubleboss00@gmail.com>
2013-09-19 17:25:17 -0400

    Fox class updated

--- src/main/java/net/supersmashcraft/Classes/ClassFox.java
+++ src/main/java/net/supersmashcraft/Classes/ClassFox.java
@@ -7,10 +7,10 @@ public class ClassFox extends SSCClass {
    
    public ClassFox() {
       super("Fox");
-      armor(Material.LEATHER_BOOTS, Color.fromRGB(255, 161, 161));
-      armor(Material.LEATHER_LEGGINGS, Color.fromRGB(255, 161, 161));
-      armor(Material.LEATHER_CHESTPLATE, Color.fromRGB(255, 161, 161));
-      armor(Material.LEATHER_HELMET, Color.fromRGB(255, 161, 161));
+      armor(Material.LEATHER_BOOTS, Color.fromRGB(247, 243, 10));
+      armor(Material.LEATHER_LEGGINGS, Color.fromRGB(255, 255, 255));
+      armor(Material.LEATHER_CHESTPLATE, Color.fromRGB(53, 191, 40));
+      armor(Material.LEATHER_HELMET, Color.fromRGB(255, 255, 255));
       item(Material.GHAST_TEAR);
       // I have to change items and armor color still
       // Fox has two events, ask on skype if your finishing fox.
