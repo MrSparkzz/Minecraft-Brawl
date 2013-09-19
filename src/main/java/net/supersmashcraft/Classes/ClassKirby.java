0c6738556a287ba77af953c8a091e9763d600670
Paul <ultimate@burngames.net>
2013-09-19 16:48:53 -0500

    Ghast tear

--- src/main/java/net/supersmashcraft/Classes/ClassKirby.java
+++ src/main/java/net/supersmashcraft/Classes/ClassKirby.java
@@ -11,6 +11,7 @@ public class ClassKirby extends SSCClass {
       armor(Material.LEATHER_LEGGINGS, Color.fromRGB(255, 161, 161));
       armor(Material.LEATHER_CHESTPLATE, Color.fromRGB(255, 161, 161));
       armor(Material.LEATHER_HELMET, Color.fromRGB(255, 161, 161));
+      item(Material.GHAST_TEAR);
    }
    
 }
