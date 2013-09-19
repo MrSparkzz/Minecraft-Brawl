d8233ed4b8aad93ac95aad68dc0a4d20c53ae59d
maxthelinkfan <maxlikeabaws@gmail.com>
2013-09-18 22:02:15 -0500

    Added Mario
    Not done, just commiting before i have to go to bed. :)

---
+++ src/main/java/net/supersmashcraft/Classes/ClassMario.java
@@ -0,0 +1,24 @@
+package net.supersmashcraft.Classes;
+
+import org.bukkit.Color;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+
+public class ClassMario extends SSCClass {
+   
+   public ClassMario() {
+      super("Mario");
+      item(Material.CACTUS);
+      ItemStack helm = colorArmor(Material.LEATHER_HELMET, Color.fromRGB(110, 80, 0));
+      ItemStack chest = colorArmor(Material.LEATHER_CHESTPLATE, Color.fromRGB(48, 110, 0));
+      ItemStack leg = colorArmor(Material.LEATHER_LEGGINGS, Color.fromRGB(48, 110, 0));
+      ItemStack boot = colorArmor(Material.LEATHER_BOOTS, Color.fromRGB(110, 80, 0));
+      
+      helm.addEnchantment(Enchantment.THORNS, 1);
+      chest.addUnsafeEnchantment(Enchantment.THORNS, 4);
+      leg.addUnsafeEnchantment(Enchantment.THORNS, 4);
+      boot.addEnchantment(Enchantment.THORNS, 1);
+      this.addItem(new ItemStack[] { helm, chest, leg, boot });
+   }
+}
